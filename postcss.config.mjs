ba485ac979ab48135dc076ffc975bb211bc17e7d
Hoang Cong Phane <hoang.phancong@hdwebsoft.dev>
2024-08-09 09:42:45 +0700

    :hammer: Bugfix: fix content field in tailwindcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,14 +2,26 @@
 
 const config = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
     ...(process.env.NODE_ENV === 'production'
       ? {
           cssnano: {},
+          'postcss-flexbugs-fixes': {},
+          'postcss-preset-env': {
+            autoprefixer: {
+              flexbox: 'no-2009',
+            },
+            stage: 3,
+            features: {
+              'custom-properties': false,
+            },
+          },
           //  reduce the file size is to remove any unrequired styles from the final CSS file.
           '@fullhuman/postcss-purgecss': {
-            content: ['./src/*.?s', './src/*.?sx'], // List every file that references the classes here. See the docs for PurgeCSS about details
+            content: ['./src/**/*.{js,jsx,ts,tsx}'],
             defaultExtractor: (content) =>
               content.match(/[\w-/:]+(?<!:)/g) || [],
             safelist: ['html', 'body'],

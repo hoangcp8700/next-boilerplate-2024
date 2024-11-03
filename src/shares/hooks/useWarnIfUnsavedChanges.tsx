74a79a202f14e5258d7f0675d8e8882856307454
Hoang Cong Phan <hoangcp219@gmail.com>
2024-11-03 11:02:38 +0700

    Fix small issue

--- src/shares/hooks/useWarnIfUnsavedChanges.tsx
+++ src/shares/hooks/useWarnIfUnsavedChanges.tsx
@@ -36,6 +36,7 @@ const useWarnIfUnsavedChanges = (isDirty: boolean) => {
         anchor.removeEventListener('click', handleAnchorClick),
       );
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
